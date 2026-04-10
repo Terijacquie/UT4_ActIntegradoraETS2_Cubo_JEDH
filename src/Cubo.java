32968d70f171d29420248cfd754b690b2cc66c8f
Jacqueline E. Díaz Hadjar <terijacquie@gmail.com>
2026-04-10 16:47:39 +0100

    Atributos, constructor y métodos realizados

--- src/Cubo.java
+++ src/Cubo.java
@@ -1,3 +1,30 @@
 public class Cubo {
-    private
+    private double lado;
+
+    public Cubo(double lado) {
+        this.lado = lado;
+    }
+
+    public float calcularVolumen() {
+        return (float) (Math.pow(this.lado, 2));
+    }
+
+    public float calcularAreaBase() {
+        return (float) Math.pow(this.lado, 2);
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cubo{" +
+                "lado=" + lado +
+                '}';
+    }
 }
